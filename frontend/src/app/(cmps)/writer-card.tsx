4a4820e32a8a1bbeaaf406fd8029725f39862b98
nivb000 <bniv000@gmail.com>
2025-01-27 20:37:27 +0200

    added base code

---
+++ frontend/src/app/(cmps)/writer-card.tsx
@@ -0,0 +1,16 @@
+
+// TODO: CLICK ON WRITER IMAGE OR READ MORE NAVIGATE TO WRITER PAGE
+export const WriterCard = () => {
+    return <article className="flex writer-card">
+        <div className="writer-img">
+            writer-img
+        </div>
+        <section className="main-text">
+            <h3 className="writer-name">שם כתב מלא</h3>
+            <div className="post">
+                <p>טקסט רץ טקסט רץ  טקסט רץ טקסט רץ טקסט רץ טקסט רץ טקסט רץ טקסט רץ</p>
+                <span className="read-more">קרא-עוד</span>
+            </div>
+        </section>
+    </article>
+}
